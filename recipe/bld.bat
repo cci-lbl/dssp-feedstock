bcfcef00adaf650ec6eb035407c1750ceefb33e0
Billy K. Poon <bkpoon@lbl.gov>
2025-06-04 11:20:32 -0700

    debug

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,8 @@ cmake %CMAKE_ARGS% --trace-expand -S . -B build ^
   -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
   -DBUILD_SHARED_LIBS=ON ^
   -DINSTALL_LIBRARY=ON ^
-  -DBUILD_PYTHON_MODULE=ON
+  -DBUILD_PYTHON_MODULE=ON ^
+  -DBUILD_TESTING=ON
 
 REM cmake always creates a Debug build
 REM cmake --build build --config Release
@@ -13,7 +14,7 @@ cd ..
 cmake --install build
 
 REM copy mkdssp.pyd file to correct location
-copy build\bin\Release\mkdssp.pyd %PREFIX%\lib\mkdssp.pyd
+copy build\bin\Release\mkdssp.pyd %SP_DIR%\mkdssp.pyd
 
 REM remove extra files in bin
 del \S \Q %PREFIX%\bin\msvcp*.dll
@@ -23,3 +24,5 @@ del \S \Q %PREFIX%\bin\concrt140.dll
 set LIBCIFPP_DATA_DIR=%PREFIX%\share\libcifpp
 ctest --output-on-failure --test-dir build -C Release
 dir
+
+%PYTHON% -c "import sys; print(sys.path)"
